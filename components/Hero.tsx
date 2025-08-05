2d404f23e0b5afb283bb1b399d8b79fc8422ac33
akilfarzan <akilfarzan@gmail.com>
2025-08-05 12:34:33 +0930

    Updated Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -78,7 +78,7 @@ export default function Hero() {
                 <Star key={i} className="w-4 h-4 fill-yellow-400 text-yellow-400" />
               ))}
             </div>
-            <span className="text-sm text-gray-600">Your ll-in-One Admin, Stock Controller and Invoice Generator</span>
+            <span className="text-sm text-gray-600">Your All-in-One Admin, Stock Controller and Invoice Generator</span>
           </motion.div>
 
           {/* Main Headline */}
