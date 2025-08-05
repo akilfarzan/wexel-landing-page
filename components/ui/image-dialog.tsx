56a26a3bae78ebea4cb70f1e5b80399f0b4e21bf
akilfarzan <akilfarzan@gmail.com>
2025-08-05 15:49:12 +0930

    Fix Component Export and Accessibility Issues

--- components/ui/image-dialog.tsx
+++ components/ui/image-dialog.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import * as Dialog from '@radix-ui/react-dialog';
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
 import { X } from 'lucide-react';
 
 interface ImageDialogProps {
@@ -10,25 +16,22 @@ interface ImageDialogProps {
 
 export function ImageDialog({ src, alt, children }: ImageDialogProps) {
   return (
-    <Dialog.Root>
-      <Dialog.Trigger asChild>
+    <Dialog>
+      <DialogTrigger asChild>
         {children}
-      </Dialog.Trigger>
-      <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 bg-black/80 backdrop-blur-sm z-50" />
-        <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 max-w-[90vw] max-h-[90vh] p-4">
-          <div className="relative">
-            <img
-              src={src}
-              alt={alt}
-              className="max-w-full max-h-[80vh] object-contain rounded-lg shadow-2xl"
-            />
-            <Dialog.Close className="absolute -top-2 -right-2 bg-white rounded-full p-2 shadow-lg hover:bg-gray-100 transition-colors">
-              <X className="w-4 h-4" />
-            </Dialog.Close>
-          </div>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+      </DialogTrigger>
+      <DialogContent className="max-w-4xl w-full p-0 bg-transparent border-none shadow-none">
+        <VisuallyHidden>
+          <DialogTitle>{alt}</DialogTitle>
+        </VisuallyHidden>
+        <div className="relative">
+          <img
+            src={src}
+            alt={alt}
+            className="w-full h-auto max-h-[90vh] object-contain rounded-lg"
+          />
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 }
