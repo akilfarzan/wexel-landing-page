b6bdcb7fd0e53e0739a1667be9da7dad103cd9a6
akilfarzan <akilfarzan@gmail.com>
2025-08-05 16:58:18 +0930

    Updated CustomizationSection.tsx

--- components/CustomizationSection.tsx
+++ components/CustomizationSection.tsx
@@ -111,7 +111,7 @@ export default function CustomizationSection() {
                   <img
                     src="/Wexel solution.png"
                     alt="Wexel customizable workshop solution interface"
-                    className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
+                    className="w-full h-full object-contain transition-transform duration-300 group-hover:scale-105"
                   />
                   {/* Overlay gradient */}
                   <div className="absolute inset-0 bg-gradient-to-tr from-blue-900/20 to-transparent" />
