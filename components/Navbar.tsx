b87990d132d75bb8370f63caf9a135762ed5eee9
akilfarzan <akilfarzan@gmail.com>
2025-08-05 18:38:16 +0930

    Updated Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
             <img
               src="/logo.gif"
               alt="Wexel Logo"
-              className="h-40 w-auto"
+              className="h-20 w-auto"
             />
           </div>
 
